c7e0083a390ab108d01c43f9ff91b44e269c6a9e
prgMgava <prgmgava@hotmail.com>
2022-11-08 21:54:54 -0300

    new sql script

--- mock-database.sql
+++ mock-database.sql
@@ -1,41 +1,58 @@
-INSERT INTO "user" ("username", "password", "admin")
-VALUES
-('admin1', '$2b$10$GgX8HPoO1gdpZ18pxzGcge1gHAIbnrs.dp4MWRMCjApcd9mRZO3ve', TRUE),
-('admin2', '$2b$10$GgX8HPoO1gdpZ18pxzGcge1gHAIbnrs.dp4MWRMCjApcd9mRZO3ve', TRUE);
+INSERT INTO "user" (username, email, password, admin, "super_admin", cellphone, is_deleted)
+VALUES ('superAdmin', 'tes@email.com', '1234', TRUE, TRUE, '2712345678',FALSE),
+ ('admin1', 'tes1@email.com', '1234', TRUE, FALSE, '15123456789', FALSE),
+ ('admin2', 'tes2@email.com', '1234', TRUE, FALSE, '9998765432', FALSE),
+ ('admin3', 'tes3@email.com', '1234', TRUE, FALSE,'', FALSE)
+ 
+ INSERT INTO "user" (username, email, password, admin, "super_admin", cellphone, is_deleted, band_visibility)
+ VALUES ('musico', 'tes4@email.com', '1234', FALSE, FALSE, '5532165000',FALSE, 2);
 
-INSERT INTO band ("name", owner)
+UPDATE "user" SET "password" = '$2b$10$Mr7b0LTr33H4rNfrfQf5OOS4WDkK6PHQEe5FfNSyp4DVPWA/hDYt2'
+
+
+INSERT INTO band  ("name", email, "owner", cellphone, is_deleted)
 VALUES 
-('KISS', 5),
-('AC/DC', 5),
-('Banda Dejavu', 5),
-('Calipso', 5),
-('Aviões do forro', 5),
-('Paramore', 5),
-('Evanescence', 6),
-('KPOP', 6),
-('Jackson 5', 6),
-('Anita', 6),
-('Gustavo Lima', 6);
+('AC/DC', 'acdc@emai.com', 2, '27123456789', FALSE), 
+('Kiss', 'kiss@email.com', 3, '2798765421', FALSE),
+('Banda Calipso', 'calpso@email.com', 4, '9999990000', FALSE),
+('Banda Dejavu', 'test@email.com', 2, '5555550000', FALSE),
+('Evanescence', 'ev@email.com', 2, '6666660000', FALSE),
+('Orchestra de Brasília', 'df@email.com', 2, '3339990000', FALSE);
 
-INSERT INTO label ("title", "color")
+INSERT INTO "label" (title, color, is_deleted)
 VALUES
-('Agendado', 'FF0000'),
-('TV/RADIO', 'FF00EF'),
-('Teatro', '8000FF'),
-('Reservado', '0033FF'),
-('Contrato', '00EFFF'),
-('Churrasquinho', '00FF66'),
-('Participação', 'DEFF00'),
-('Ginga', 'FF4400'),
-('Ensaio interno', 'E6FF00'),
-('Confirmado', '000000'),
-('Cancelado', 'FFA200'),
-('Concluido', '00FFAB');
+('maiusculas', '#FF0000', FALSE),
+('minusculas', '#ff5500', FALSE),
+('em negociação', '#FFFF00', FALSE),
+('show fechado', '#00FF00', FALSE),
+('participação', '#1E90FF', FALSE),
+('gravação', '#FF69B4', FALSE)
 
+INSERT INTO appointment ("title", cellphone, status, id_band , "start_date" , "end_date", 
+street, district, state, city, "address_number" , "address_complement", id_label)
+VALUES
+('Meu evento', '123456789', 'agendado', 1, '2022-10-10T13:18:00',
+'2022-10-10T15:18:00', 'Rua Alameda',
+'Bairro Republica', 'ES', 'Vitória', '123', 'Proximo a radio',1  ),
+('Meu evento 2', '123456789', 'concluido', 2, '2022-10-12T13:18:00',
+'2022-10-12T15:18:00', 'Rua Alameda',
+'Bairro Republica', 'ES', 'Vitória', '123', 'Proximo a radio' ,1 ),
+('Meu evento 3', '123456789', 'concluido', 3, '2022-10-01T13:18:00',
+'2022-10-01T15:18:00', 'Rua Alameda',
+'Bairro Republica', 'ES', 'Vitória', '123', 'Proximo a radio' ,2 ),
+('Meu evento 4', '123456789', 'concluido', 4, '2022-10-02T13:18:00',
+'2022-10-02T15:18:00', 'Rua Alameda',
+'Bairro Republica', 'ES', 'Vitória', '123', 'Proximo a radio' ,3 ),
+('Meu evento 5', '123456789', 'concluido', 5, '2022-10-03T13:18:00',
+'2022-10-03T15:18:00', 'Rua Alameda',
+'Bairro Republica', 'ES', 'Vitória', '123', 'Proximo a radio' ,4 ),
+('Meu evento 6', '123456789', 'concluido', 5, '2022-10-03T13:18:00',
+'2022-10-03T15:18:00', 'Rua Alameda',
+'Bairro Republica', 'ES', 'Vitória', '123', 'Proximo a radio' ,4 )
 
-Novas atualizacoes:
-1 - Tooltip mostrando informações quando passar o mouse por cima do evento;
-2 - Novos campos no formulario: (bilheteria, cache), evento (corporativo, show, participação);
-3 - FILTROS: por artistas
-4 - Nova tela: lista com os compromissos filtrados por mes
-
+INSERT INTO appointment ("title", cellphone, status, id_band , "start_date" , "end_date", 
+street, district, state, city, "address_number" , "address_complement", id_label, "event", "money", company_name, contractor, company_cellphone, company_contact, company_email, emphasis, observations, creator)
+VALUES
+('Meu evento', '123456789', 'agendado', 1, '2022-11-08T13:18:00',
+'2022-11-08T15:18:00', 'Rua Alameda',
+'Bairro Republica', 'ES', 'Vitória', '123', 'Proximo a radio',1, 'Show', 'Cache', 'Minha empresa', 'Silvio Santos', '(27) 99999-9999', 'Contato da empresa', 'company@email.com', 'Destaque exemplo', 'Evento muito legal', 'admin1'  )
