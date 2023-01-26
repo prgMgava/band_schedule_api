2e0943a2e7eb0b471b61da6452534cf8221d2d5b
prgMgava <78923115+prgMgava@users.noreply.github.com>
2023-01-26 17:19:54 -0300

    Revert ssl

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 require("dotenv/config");
 var cors = require("cors");
-var fs = require("fs");
-var http = require("http");
-var https = require("https");
-var privateKey = fs.readFileSync(
-  "/etc/ssl/private/nginx-selfsigned.key",
-  "utf8"
-);
-var certificate = fs.readFileSync(
-  "/etc/ssl/certs/nginx-selfsigned.crt",
-  "utf8"
-);
-var credentials = { key: privateKey, cert: certificate };
 
 const express = require("express");
 const routes = require("./routes");
@@ -20,9 +8,6 @@ const compression = require("compression");
 require("./database");
 
 const app = express();
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-
 app.use(cors());
 app.use(compression());
 app.use(express.json());
@@ -43,6 +28,5 @@ app.use(routes);
 // 		},
 // 	});
 // })
-httpServer.listen(3332);
-httpsServer.listen(3333);
-//app.listen(process.env.API_PORT || 3333);
+
+app.listen(process.env.API_PORT || 3333);
